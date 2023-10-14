36d27a7018c6cbe6ddb1d0a09c8f79e8756d7527
unknown <shadi.wsd@gmail.com>
2023-10-15 02:36:54 +0400

    check super admin auth for create super admin

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -22,7 +22,8 @@ router.use(bodyParser.urlencoded({extended: true}))
 
 
 //admin begin
-router.post("/create-super-admin", checkDevCreateAdmin, catchAsyncError(createSuperAdminAccount))//dev
+// checkDevCreateAdmin
+router.post("/create-super-admin", checkSuperAdminAuth, catchAsyncError(createSuperAdminAccount))//dev
 // router.post("/passport-checkIn", checkAdminAuth, catchAsyncError(passportCheckIn))//delete
 router.post("/create-admin", checkAdminAuth, catchAsyncError(createAdmin))//super admin
 router.post("/create-practice-account", checkAdminAuth, catchAsyncError(createPracticeUser))//admin
