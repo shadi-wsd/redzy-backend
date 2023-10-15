bcbb1b004b367adee74c22833d81f5b5d7e988b7
unknown <shadi.wsd@gmail.com>
2023-10-15 20:42:18 +0400

    change database

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = (DATABASE_URL) => {
     try {
         const DB_OPTION = {
-            dbName: "thumbtack"
+            dbName: "rezdy"
         }
         mongoose.connect(DATABASE_URL, DB_OPTION);
         console.log("Database connceted Successfully");
